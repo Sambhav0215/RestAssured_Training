5eebe4c1765413e451f100d8492a95feda8139cd
Sambhav0215 <itzsambhav02@gmail.com>
2026-06-20 00:19:56 +0530

    Day03 Done

--- src/test/java/Day03/QueryAndPathParameters.java
+++ src/test/java/Day03/QueryAndPathParameters.java
@@ -2,9 +2,27 @@ package Day03;
 
 import org.testng.annotations.Test;
 
-public class QueryAndPathParameters {
-    @Test
-    public void testCookies(){
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
+public class QueryAndPathParameters {
+
+    @Test
+    public void testQueryAndPathParameters(){
+
+        given()
+                .header("x-api-key","free_user_3EL4FeP7G9SNVZlTS3D8roEauh9")
+                .pathParams("paths","users")
+                .queryParam("page",2)
+                .queryParam("id",5)
+
+                .when()
+                .get("https://reqres.in/api/{paths}")
+
+                .then()
+                .statusCode(200)
+                .body("data.id", equalTo(5))
+                .log().all();
     }
+
 }
