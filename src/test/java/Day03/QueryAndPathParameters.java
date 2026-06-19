7f1af4191a329095098c48b46aee5145a5fd422b
Sambhav0215 <itzsambhav02@gmail.com>
2026-06-19 23:44:09 +0530

    "merged query file with main"

--- src/test/java/Day03/QueryAndPathParameters.java
+++ src/test/java/Day03/QueryAndPathParameters.java
@@ -1,4 +1,28 @@
 package Day03;
 
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 public class QueryAndPathParameters {
+
+    @Test
+    public void testQueryAndPathParameters(){
+
+        given()
+                .header("x-api-key","free_user_3EL4FeP7G9SNVZlTS3D8roEauh9")
+                .pathParams("path","users")
+                .queryParam("page",2)
+                .queryParam("id",5)
+                .when()
+                .get("https://reqres.in/api/{path}")
+
+                .then()
+                .statusCode(200)
+                .log().all();
+
+    }
+
+
 }
